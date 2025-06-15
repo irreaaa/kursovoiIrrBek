5eccf64830ba390740b3040a894cb89fdd1e5469
irreaaa <irispbmink@gmail.com>
2025-06-15 20:59:45 +0300

    pr

--- src/main/kotlin/model/DataRepository.kt
+++ src/main/kotlin/model/DataRepository.kt
@@ -24,58 +24,76 @@ object DataRepository {
             listOf(
                 Sneacker(
                     id = 1,
-                    name = "Nike Air Max",
-                    description = "sneakers",
-                    price = 732.0,
-                    imageUrl = "mainsneakers",
-                    category = "Tennis",
+                    name = "Розовые пионы",
+                    description = "папм",
+                    price = 6100.0,
+                    imageUrl = "peonia",
+                    category = "Монобукеты",
                     isPopular = true
                 ),
                 Sneacker(
                     id = 2,
-                    name = "Nike Winflo",
-                    description = "Running",
-                    price = 850.0,
-                    imageUrl = "mainsneakers",
-                    category = "Running",
+                    name = "Воздушный зефир",
+                    description = "пам",
+                    price = 5000.0,
+                    imageUrl = "comb_1",
+                    category = "Сборные букеты",
                     isPopular = false
                 ),
                 Sneacker(
                     id = 3,
-                    name = "Nike Zoom Vomero",
-                    description = "donkey",
-                    price = 999.0,
-                    imageUrl = "mainsneakers",
-                    category = "Outdoor",
+                    name = "Розы в коробке",
+                    description = "пам",
+                    price = 2999.0,
+                    imageUrl = "box_rose",
+                    category = "Букеты в коробке",
                     isPopular = true
                 ),
                 Sneacker(
                     id = 4,
-                    name = "Nike Revolution",
+                    name = "Кустовые розы розовые",
                     description = "zebra",
-                    price =239.0,
-                    imageUrl = "mainsneakers",
-                    category = "Tennis",
+                    price =3390.0,
+                    imageUrl = "mono_rose",
+                    category = "Монобукеты",
                     isPopular = false
                 ),
                 Sneacker(
                     id = 5,
-                    name = "Nike Force",
+                    name = "Красные тюльпаны 25шт",
                     description = "monkey",
-                    price = 855.0,
-                    imageUrl = "mainsneakers",
-                    category = "Outdoor",
+                    price = 4700.0,
+                    imageUrl = "mono_toulpin_red",
+                    category = "Монобукеты",
                     isPopular = true
                 ),
                 Sneacker(
                     id = 6,
-                    name = "Nike Borough",
+                    name = "Розовые тюльпаны 25шт",
                     description = "pig",
-                    price = 777.0,
-                    imageUrl = "mainsneakers",
-                    category = "Tennis",
+                    price = 4600.0,
+                    imageUrl = "mono_toulip_pink",
+                    category = "Монобукеты",
                     isPopular = true
-                )
+                ),
+                Sneacker(
+                    id = 7,
+                    name = "Букет в коробке",
+                    description = "пам",
+                    price = 2500.0,
+                    imageUrl = "box_1",
+                    category = "Букеты в коробке",
+                    isPopular = false
+                ),
+                Sneacker(
+                    id = 8,
+                    name = "Букет в коробке 2",
+                    description = "пам",
+                    price = 2999.0,
+                    imageUrl = "box_3",
+                    category = "Букеты в коробке",
+                    isPopular = true
+                ),
             )
         )
     }
